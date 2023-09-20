0487acc01c25f7b33c86efbdc6dc41e6ca1f00d8
Kyada Pavan <kyadapavan0016@gmail.com>
2023-09-20 23:02:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ const author="Pavan";
 3) const
 
 
-var :-  var is a globally scoped variable and it can be updated and 
-        re-declared within its scope. 
+var :-  var is a globally scoped variable and it can be updated and re-declared within its scope. 
 
-let :-  let is block scoped variable and it can be updated but it can not          be re-declared. 
+let :-  let is block scoped variable and it can be updated but it can not be re-declared. 
 
-const :- the value stored with the help of const can not be changed. const          can neither be upadted or re-declared.
+const :- the value stored with the help of const can not be changed. const can neither be upadted or re-declared.
+          const must be initialized during declaration unlike var and let.
 
 */
