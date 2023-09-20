d0dc42fe850a79c869016bd3987c57368db733da
Kyada Pavan <kyadapavan0016@gmail.com>
2023-09-20 23:37:15 +0530

    Update DataTypes.js

--- DataTypes.js
+++ DataTypes.js
@@ -1,1 +1,21 @@
+let a = null ; // Null defines empty value
+let b = 18;    // Number defines any numeric value
+let c = true;  // Boolean either defines true or false
+let d = BigInt(458624); // Bigint define any larger numeric value
+let e = "Pavan" ;  // String defines multiple character
+let f = Symbol("This is a symbol");  // It is used to define any symbol
+let g ; // If we don't pass any value to the variable it is by default undefined
+
+console.log(a,b,c,d,e,f,g);
+console.log(typeof e); // typeof returns which type of data type it is
+
+
+
+const obj1 = {
+  "Name": "Pavan",
+  "Role": "Front-End",
+  "Age": 21
+}
+console.log(obj1["Name"]);
+
 
