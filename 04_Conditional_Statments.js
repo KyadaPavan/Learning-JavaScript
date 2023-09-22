487c9dd3428b96a7e93e37a0b7e4f9c23434979a
Kyada Pavan <kyadapavan0016@gmail.com>
2023-09-23 01:06:20 +0530

    Update 04_Conditional_Statments.js

--- 04_Conditional_Statments.js
+++ 04_Conditional_Statments.js
@@ -1,1 +1,32 @@
+const prompt = require('prompt-sync')();
 
+// If else statments
+let age = prompt("Enter your age: ");
+console.log(age);
+
+if(age>=18 && age<60){
+    console.log("You are elligble");
+}
+else if(age>=60){
+    console.log("You are Senior Citizen");
+}
+else{
+    console.log("You are not elligble");
+}
+
+// Switch Case 
+
+const Grade = prompt("Enter your Grade: ");
+switch (Grade){
+    case 'A':
+        console.log("Very Good");
+        break;
+    case 'B':
+        console.log("Good");
+        break;
+    case 'C':
+        console.log("Pass");
+        break;
+    default:
+        console.log("Fail")
+}
