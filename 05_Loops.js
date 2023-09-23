d3b0991e90a20e7f1b11e44eb2db216fe2f752d4
Kyada Pavan <kyadapavan0016@gmail.com>
2023-09-23 22:30:21 +0530

    Update 05_Loops.js

--- 05_Loops.js
+++ 05_Loops.js
@@ -1,1 +1,19 @@
 
+const prompt = require('prompt-sync')()
+
+// Program to find the sum of first n natural number
+let sum =0;
+let n = prompt("Enter The Number Upto which you want it's summation: ");
+n = Number.parseInt(n);
+if(n<0){
+    console.log("You have enterd an invalid number");
+}
+else if (n==0){
+    console.log(0) ;
+}
+else{
+    for(let i=1; i<=n;i++){
+         sum += i ;
+    }       
+    console.log(sum);
+}
