f57485e3eaacf3ba1e12c8455656d2ab899513ed
Kyada Pavan <kyadapavan0016@gmail.com>
2023-09-23 22:32:38 +0530

    Update 05_Loops.js

--- 05_Loops.js
+++ 05_Loops.js
@@ -17,3 +17,33 @@ else{
     }       
     console.log(sum);
 }
+// Program for multiplication tabel 
+let mul = prompt("Enter the number for which you want multiplication table: ");
+mul = Number.parseInt(mul);
+for(let j=1; j<=10 ; j++){
+   console.log(mul + "x" + j + "=" + (mul*j)  ) 
+}
+
+// Program to find square of the number
+
+let sqr = prompt("Enter the number for which you want to find it's square: ");
+sqr = Number.parseInt(sqr);
+console.log("Square of " + sqr + " is " + sqr*sqr);
+
+// for in loop
+
+let marks = {
+    Ramu: 45 ,
+    Shyamu: 48,
+    Babu: 36
+}
+for(let a in marks){
+    console.log(a + " got " + marks[a] + " Marks.");
+}
+
+
+// for of loop
+
+for (b of "Pavan"){
+    console.log(b)
+}
