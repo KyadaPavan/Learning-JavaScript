1748ad4aa2964ade0fb519572a938027988cf472
Kyada Pavan <kyadapavan0016@gmail.com>
2023-09-26 22:06:43 +0530

    Update 09_Map_Filter_Reduce.js

--- 09_Map_Filter_Reduce.js
+++ 09_Map_Filter_Reduce.js
@@ -1,1 +1,27 @@
 
+// map 
+
+const a = [1,8,6,3,5,4,10];
+let b = a.map((value)=>{
+    return value + 1;
+    
+})
+
+console.log(b);
+
+// filter
+
+let c = a.filter((func)=>{
+    if(func%10 == 0 ){
+        return func
+    }
+    
+})
+console.log(c);
+
+// reduce
+
+let d = a.reduce((mult1 , mult2)=>{
+    return mult1 + mult2;
+})
+console.log(d);
