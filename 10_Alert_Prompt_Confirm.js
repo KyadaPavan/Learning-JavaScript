f0470d7706c8d58cd5db8cacc2331b633a462060
Kyada Pavan <kyadapavan0016@gmail.com>
2023-09-27 23:20:15 +0530

    Update 10_Alert_Prompt_Confirm.js

--- 10_Alert_Prompt_Confirm.js
+++ 10_Alert_Prompt_Confirm.js
@@ -1,1 +1,13 @@
 
+alert("hello, this is an alert")
+
+let a = prompt("Enter value the of a");
+
+
+let Name = confirm("Do you want write the entered number?")
+if(Name){
+    document.write("You enterd the number " + a + ".")
+}
+else{
+    document.write("Please, allow me to write.")
+}
