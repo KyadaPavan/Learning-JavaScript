cc0116295138fc5f4d08a1509a5020fcdd774626
Kyada Pavan <kyadapavan0016@gmail.com>
2023-09-29 18:34:45 +0530

    Update 11_Searching_in_DOM.js

--- 11_Searching_in_DOM.js
+++ 11_Searching_in_DOM.js
@@ -1,1 +1,21 @@
+// let ctitle = document.getElementsByClassName("card-title")[0];
+// ctitle.style.color = "green" ;
+
+let ctitles = document.querySelectorAll(".card-title");
+ctitles[0].style.color = "purple" ;
+ctitles[1].style.color = "green" ;
+ctitles[2].style.color = "red" ;
+
+let go = document.querySelectorAll(".this");
+go[0].style.background = "purple";
+go[1].style.background = "green";
+go[2].style.background = "red";
+
+
+let goboder = document.querySelectorAll(".btn");
+goboder[1].style.borderColor = "purple";
+goboder[2].style.borderColor = "green";
+goboder[3].style.borderColor = "red";
+
+
 
